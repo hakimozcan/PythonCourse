4bc30ed7a11421f62147e40b6c5606656391f123
abdulhakim özcan <aozcan15@ku.edu.tr>
2021-02-20 19:55:18 +0300

    Initial Commit

---
+++ employee.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 01:10:34 2021
+
+@author: User
+"""
+
+import requests
+
+
+class Employee:
+    """A sample Employee class"""
+
+    raise_amt = 1.05
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    
