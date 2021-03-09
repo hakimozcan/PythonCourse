cefe110717924e96b5a69471f4b2917f699d3675
hakimozcan <aozcan15@ku.edu.tr>
2021-03-09 23:09:48 +0300

    first hw for qmbu450:advanced data analysis in python

--- homework1.py
+++ homework1.py
@@ -163,24 +163,6 @@ class MutualFund:
                 raise TypeError("Make sure to enter a string for ticker symbol")
         except:
             print("Please enter a string ticker symbol.")    
-    
-portfolio = Portfolio()
-portfolio.addCash(300.50) #Adds cash to the portfolio
-
-s = Stock(20, "HFH") #Create Stock with price 20 and symbol "HFH"
-portfolio.buyStock(5, s) #Buys 5 shares of stock s
-mf1 = MutualFund("BRT") #Create MF with symbol "BRT"
-mf2 = MutualFund("GHT") #Create MF with symbol "GHT"
-portfolio.buyMutualFund(10.3, mf1) #Buys 10.3 shares of "BRT"
-
-portfolio.buyMutualFund(2, mf2) #Buys 2 shares of "GHT"
-
-print(portfolio) #Prints portfolio  #cash: $140.50  #stock: 5 HFH  #mutual funds: 10.33 BRT # 2 GHT
-
-portfolio.sellMutualFund("BRT", 3) #Sells 3 shares of BRT
-portfolio.sellStock("HFH", 1) #Sells 1 share of HFH
-portfolio.withdrawCash(50) #Removes $50
-portfolio.history() #Prints a list of all transactions  #ordered by time
 
 
 
